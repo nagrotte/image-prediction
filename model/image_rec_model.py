f8d0a7041b39125d89fb9aa54fa84a5980c41dea
Nag Rotte <nag.rotte@gmail.com>
2024-02-15 18:06:20 -0600

    Train model

--- model/image_rec_model.py
+++ model/image_rec_model.py
@@ -35,3 +35,37 @@ prediction = Dense(len(folders), activation ='softmax')(X)
 model = Model(inputs=resnet152V2.input, outputs=prediction)
 
 model.summary()
+
+
+model.compile (
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics = ['accuracy']
+)
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+training_set = train_datagen.flow_from_directory('Datasets/train',
+                                                 target_size = (224, 224),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('Datasets/test',
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+r = model.fit_generator(
+  training_set,
+  validation_data=test_set,
+  epochs=20,
+  steps_per_epoch=len(training_set),
+  validation_steps=len(test_set)
+)
